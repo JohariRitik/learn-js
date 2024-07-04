9741dfdc0e8c96404cbdd82550b205efb48f3b1d
JohariRitik <77263490+JohariRitik@users.noreply.github.com>
2024-07-04 05:43:38 +0000

    learning the basics of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 11298
+let accountEmail="ritikjohari411@gmail.com";
+var  accountPassword="12345"
+accountCity="Mainpuri"
+console.log(accountId);
+console.table([accountId,accountPassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello");does not work in node js only for browser
+
+let name="Ritik" //string datatype
+let age=18 //number 2^53 //big int for larger numbers
+let isLoggedIn=false //boolean
+let i=null //null is a standalone vaue =>object
+let j //undefined no value is defined
+//symbol =>unique
+//object
+
+console.log(typeof"hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
